a651cdfb9515aaa740494365fcc3a7b241da93b4
Tyjoo26 <Tyjoo21@gmail.com>
2018-07-25 13:17:48 -0600

    add relationships at model level

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,6 @@
 class Card < ApplicationRecord
+  belongs_to :customer
+
   validates_uniqueness_of :card_number
   validates_presence_of :card_number,
                         :expiration_date,
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
+  has_many :cards
+  
   validates_presence_of :first_name,
                         :last_name,
                         :address,
