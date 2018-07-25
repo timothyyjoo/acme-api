1dd9c5c1e4fa31f31191470af0fa424b01cf15e7
Tyjoo26 <Tyjoo21@gmail.com>
2018-07-25 12:34:44 -0600

    Create migration for customer and initialize schema

---
+++ db/migrate/20180725182258_create_customers.rb
@@ -0,0 +1,14 @@
+class CreateCustomers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zip
+
+      t.timestamps
+    end
+  end
+end
