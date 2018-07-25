0a8c6710a1d927a27ed60a152c6264ad86024b0b
Tyjoo26 <Tyjoo21@gmail.com>
2018-07-25 12:40:14 -0600

    Add validations for city and state in customer model, all customer validations pass

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,7 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name,
                         :last_name,
                         :address,
+                        :city,
+                        :state,
                         :zip
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -8,5 +8,41 @@ RSpec.describe Customer, type: :model do
 
       expect(customer).to be_valid
     end
+
+    it "will not save a customer without first_name" do
+      customer = Customer.new(last_name: "Jones", address: "2120 Wimbledon Court", city: "Modesto", state: "CA", zip: "95355")
+
+      expect(customer).to_not be_valid
+    end
+
+    it "will not save a customer without last_name" do
+      customer = Customer.new(first_name: "Jones", address: "2120 Wimbledon Court", city: "Modesto", state: "CA", zip: "95355")
+
+      expect(customer).to_not be_valid
+    end
+
+    it "will not save a customer without address" do
+      customer = Customer.new(first_name: "Jones", last_name: "Jones", city: "Modesto", state: "CA", zip: "95355")
+
+      expect(customer).to_not be_valid
+    end
+
+    it "will not save a customer without city" do
+      customer = Customer.new(first_name: "Jones", last_name: "Jones", address: "123 Modesto Lane", state: "CA", zip: "95355")
+
+      expect(customer).to_not be_valid
+    end
+
+    it "will not save a customer without state" do
+      customer = Customer.new(first_name: "Jones", last_name: "Jones", address: "123 Modesto Lane", city: "Culver City", zip: "95355")
+
+      expect(customer).to_not be_valid
+    end
+
+    it "will not save a customer without zip" do
+      customer = Customer.new(first_name: "Jones", last_name: "Jones", address: "123 Modesto Lane", city: "Culver City", state: "CA" )
+
+      expect(customer).to_not be_valid
+    end
   end
 end
