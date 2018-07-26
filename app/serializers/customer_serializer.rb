bf370f26bfaf04b0832b51dfe5fd706de0b524be
Tyjoo26 <Tyjoo21@gmail.com>
2018-07-26 16:20:06 -0600

    Expose additional info

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,5 +1,10 @@
 class CustomerSerializer < ActiveModel::Serializer
   attributes :id,
              :first_name,
-             :last_name
+             :last_name,
+             :address,
+             :city,
+             :state,
+             :zip,
+             :token
 end
