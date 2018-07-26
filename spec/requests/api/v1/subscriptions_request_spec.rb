f9b3dd0c2e7ecb37e38ddd7ce9d824eec458ad26
Tyjoo26 <Tyjoo21@gmail.com>
2018-07-26 15:19:52 -0600

    request spec tests0

--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -30,7 +30,32 @@ describe "subscriptions api" do
     expect(response).to be_success
 
     expect(customer.first_name).to eq(subscription_params[:customer][:first_name])
+  end
 
+  it "invalid post request returns empty response.body" do
+    create_list(:plan, 3)
+    subscription_params = {
+    	"customer": {
+    		"first_name": "Jim",
+    		"last_name": "Jones",
+    		"address": "2120 Wimbeldon Court",
+    		"city": "Modesto",
+    		"state": "CA",
+    		"zip": 95355
+    	},
+    	"amount": "4900",
+    	"card_number": "42424242424242421",
+    	"cvv": "123",
+    	"expiration_month": "01",
+    	"expiration_year": "2024",
+    	"zip_code": "11754",
+    	"plan_id": 2
+
+    }
+
+    post "/api/v1/subscriptions", {params:  subscription_params }
+
+    expect(response.body).to eq('')
   end
 
 
