8891dbb9ae207b83229c3795b8f7d196a3efd56f
Tyjoo26 <Tyjoo21@gmail.com>
2019-01-24 13:54:37 -0600

    refactored subscription model by doing validations with active record queries

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,15 +2,29 @@ class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :plan
 
-  def self.validate_pricing
-      { "1" =>  [1999, "1999"], "2" =>  ["4900", 4900], "3" =>  ["9900", 9900]}
-  end
-
-  def self.check_amount_with_plan(object)
+  def self.set_plan_and_amount(object)
     plan = object[:plan_id]
     amount = object[:amount]
 
-    Subscription.validate_pricing[plan].nil? ? false :
-    Subscription.validate_pricing[plan].include?(amount)
+    self.valid_plan?(plan, amount)
   end
+
+  def self.valid_plan?(plan_id, amount)
+    begin
+      plan = Plan.find(plan_id)
+    rescue ActiveRecord::RecordNotFound
+      return {:error => "Plan_id does not exist in database"}
+    end
+    self.check_amount_against_plan(plan, amount)
+  end
+
+  def self.check_amount_against_plan(plan, amount)
+    find_by_amount = Plan.find_by(price: amount)
+    if plan.id == find_by_amount.id
+      true
+    else
+      false
+    end
+  end
+
 end
