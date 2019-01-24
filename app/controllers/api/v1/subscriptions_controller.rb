0b530966614934d8f14ac2645ab80ae0fef5bc4c
Tyjoo26 <Tyjoo21@gmail.com>
2019-01-24 12:13:19 -0600

    Refactored subscriptions controller logic into controller logic, updated tests and routes

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,38 +1,3 @@
 class Api::V1::SubscriptionsController < ApplicationController
 
-
-  def create
-    response = FakepayService.post_json('/purchase', params).body
-    check_subscription(response)
-  end
-
-  def check_subscription(object)
-   if Subscription.check_amount_with_plan(params) == true
-     check_transaction(object)
-   else
-     render json: {:error =>  "The payment amount does not align with the price of the selected plan. Please check your POST request"}
-   end
-  end
-
-  def check_transaction(object)
-    formatted = FakepayService.parse_json(object)
-    if formatted[:success] == false
-      render json: {:error =>  "Your transaction failed due to error code: #{formatted[:error_code]}. Please check the error code description"}
-    elsif formatted[:success] == true
-      customer_validation = Customer.store_token(formatted, params, customer_params)
-      render json: customer_validation
-    else
-      render json: formatted
-    end
-  end
-
-
-
-  private
-
-  def customer_params
-    params.require(:customer).permit(:first_name, :last_name, :address, :city, :state, :zip)
-  end
-
-
 end
--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe "Customer API" do
+  before :each do
+    Plan.create(name: "Bronze Box", price: "1999")
+    Plan.create(name: "Silver Box", price: "4900")
+    Plan.create(name: "Gold Box", price: "9900")
+  end
   it "sends a list of customers" do
     create_list(:customer, 5)
 
@@ -14,12 +19,100 @@ describe "Customer API" do
 
   it "sends a single customer by its id" do
     id = create(:customer).id
-    
+
     get "/api/v1/customers/#{id}"
     expect(response).to be_successful
 
     customer = JSON.parse(response.body)
 
     expect(customer["id"]).to eq(id)
+  end
+
+  it "accepts a valid POST request" do
+
+
+    customer_params = {
+      "customer": {
+        "first_name": "Jim",
+        "last_name": "Jones",
+        "address": "2120 Wimbeldon Court",
+        "city": "Modesto",
+        "state": "CA",
+        "zip": 95355
+      },
+      "amount": "4900",
+      "card_number": "42424242424242420",
+      "cvv": "123",
+      "expiration_month": "01",
+      "expiration_year": "2024",
+      "zip_code": "11754",
+      "plan_id": 2
+
+    }
+
+    post "/api/v1/customers", {params:  customer_params }
+
+    customer = Customer.last
+
+    assert_response :success
+    expect(response).to be_success
+
+    expect(customer.first_name).to eq(customer_params[:customer][:first_name])
+    expect(customer.plans.first.id).to eq(customer_params[:plan_id])
+    expect(response.body).to eq('{"status":"You have successfully loaded a customer into the database"}')
+  end
+
+  it "invalid post request returns json_message" do
+
+
+    customer_params = {
+      "customer": {
+        "first_name": "Jim",
+        "last_name": "Jones",
+        "address": "2120 Wimbeldon Court",
+        "city": "Modesto",
+        "state": "CA",
+
+      },
+      "amount": "4900",
+      "card_number": "42424242424242421",
+      "cvv": "123",
+      "expiration_month": "01",
+      "expiration_year": "2024",
+      "zip_code": "11754",
+      "plan_id": 2
+
+    }
+
+    post "/api/v1/customers", {params:  customer_params }
+
+    expect(response.body).to eq('{"error":"Your transaction was successful but there was an issue with customer details and we were unable to save the information in our database"}')
+  end
+
+  it "invalid plan_id and amount POST request returns json_message" do
+
+
+    customer_params = {
+      "customer": {
+        "first_name": "Jim",
+        "last_name": "Jones",
+        "address": "2120 Wimbeldon Court",
+        "city": "Modesto",
+        "state": "CA",
+
+      },
+      "amount": 1999,
+      "card_number": "42424242424242421",
+      "cvv": "123",
+      "expiration_month": "01",
+      "expiration_year": "2024",
+      "zip_code": "11754",
+      "plan_id": 3
+
+    }
+
+    post "/api/v1/customers", {params:  customer_params }
+
+    expect(response.body).to eq('{"error":"The payment amount does not align with the price of the selected plan. Please check your POST request"}')
   end
 end
