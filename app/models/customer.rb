900262a8801c7e4850bcf762e23dfccd20ab1ab1
Tyjoo26 <Tyjoo21@gmail.com>
2019-01-24 14:09:58 -0600

    refactored heavy method into smaller methods

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -14,16 +14,30 @@ class Customer < ApplicationRecord
 
   def self.store_token(object, params, customer_data)
     customer = Customer.new(customer_data)
-    if customer.save
+    begin
+      customer.save!
       plan = Plan.find(params[:plan_id])
-      customer.plans << plan
-      customer.token = object[:token]
-      customer.save
-      return {:status =>  "You have successfully loaded a customer into the database"}
-    else
-      return {:error =>  "Your transaction was successful but there was an issue with customer details and we were unable to save the information in our database"}
+      self.set_plan(customer,plan, object)
+    rescue ActiveRecord::RecordInvalid => e
+      return {:error => e.to_s, message: "Customer not saved to the database", status: :record_invalid}
     end
   end
+
+  def self.set_plan(customer, plan, object)
+    customer.plans << plan
+    self.set_token_and_save(customer, object)
+  end
+
+  def self.set_token_and_save (customer, object)
+    customer.token = object[:token]
+    self.save_and_return_success_hash(customer)
+  end
+
+  def self.save_and_return_success_hash(customer)
+    customer.save
+    return {:status =>  "You have successfully loaded a customer into the database"}
+  end
+
 
 
 end
